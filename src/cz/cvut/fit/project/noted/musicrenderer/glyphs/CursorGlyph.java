42512d0fb1187fbd45008121e20818538bcf0a5d
Adam Příhoda <prihoada@fit.cvut.cz>
2014-10-08 12:40:22 +0200

    cursor glyph skeleton

---
+++ src/cz/cvut/fit/project/noted/musicrenderer/glyphs/CursorGlyph.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package cz.cvut.fit.project.noted.musicrenderer.glyphs;
+
+import cz.cvut.fit.project.noted.musicrenderer.glyphs.base.Glyph;
+import java.awt.Graphics2D;
+
+/**
+ *
+ * @author Adam Příhoda
+ */
+public class CursorGlyph extends Glyph
+{
+
+    @Override
+    public void doLayout() {
+    }
+
+    @Override
+    public void paint(int x, int y, Graphics2D g) {
+
+    
+    }
+    
+}
