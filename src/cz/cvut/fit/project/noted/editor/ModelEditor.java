9d05d4c639ede3a0bfd62e9d636b108fa14a9566
vavricka <vavricka.d@gmail.com>
2014-10-11 17:31:43 -0300

    dalsi upravy v Cursor a ModelEditor

--- src/cz/cvut/fit/project/noted/editor/ModelEditor.java
+++ src/cz/cvut/fit/project/noted/editor/ModelEditor.java
@@ -1,7 +1,9 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * what to do next?
+ * 1. presun posunX/Y -> modelEditor z Cursor
+ * 2. addNote
+   3. MAYBE: measures-> udelat pole a ulozit measures pro vsechny parts
+    4. addNote pridelat pro vsechny znaky all..
  */
 package cz.cvut.fit.project.noted.editor;
 
@@ -19,22 +21,37 @@ public class ModelEditor
 {
 
     private final Cursor cursor;
-   // private final Model model;
-    //private final ScorePartwise modelHierarchy;
-    //List<ScorePartwise.Part> parts;
+    private final Model model;
+    private final ScorePartwise modelHierarchy;
+    
+    private final List<ScorePartwise.Part> parts;
+    private final List<ScorePartwise.Part.Measure> measures;
+    
+    private final int partPos;
+    private final int measurePos;
+    private final int x;
+    private final int y;
     
     
-    public ModelEditor()
+    
+    
+    
+    public ModelEditor(Model model)
     {
-       cursor = new Cursor();
+        cursor = new Cursor(model);
         
-        //this.model = model;
-        //this.modelHierarchy = model.getModelHierarchy();
+        this.model = model;
+        this.modelHierarchy = model.getModelHierarchy();
         
-        //parts = modelHierarchy.getPart();
+        // TO WHERE PUT THE NOTE
+        this.partPos = cursor.getPart();
+        this.measurePos = cursor.getMeasure();
+        this.x = cursor.getPosition_x();
+        this.y = cursor.getPosition_y();
         
-        
-        
+        this.parts = modelHierarchy.getPart(); 
+        this.measures = parts.get(partPos).getMeasure(); //CHECK
+     
     }
     
     
@@ -42,13 +59,14 @@ public class ModelEditor
     
     
     
-    private Note createNote()
+    //TODO mozna ne string ale primo nota?
+    private Note createNote(String noteType)
     {
         ObjectFactory factory = new ObjectFactory();
         
         Note note = factory.createNote();
         
-        //nastaveni noty....
+        //nastaveni noty. - switch podle STRING NOTETYPE... TODO !!!!
         //note.setPitch(blalala);
         //set type,,,..
         
@@ -56,6 +74,34 @@ public class ModelEditor
         return note;
     }
     
+    //TODO pridat parametry jakou notu atd....
+    public void addNote(String noteType)
+    {
+        
+        //CREATE NOTE 
+        Note note = createNote(noteType);
+        
+
+        //FINALLY PUT IT THERE  TODO
+        //nota bude z measures(measure) .... x,y
+        //measures.get(measurePos).adddnota(,.mdfdfsd)
+    }
+    
+    //moves Note on current cursor right,
+    //the cursor moves right also
+    public void moveToRight()
+    {
+        //move to right TODO
+        
+        
+        //if done, move cursor to this position too
+        cursor.moveToRight();
+    }
+    
+    public void moveToLeft()
+    {
+        //TODO
+    }
     
     
 }
