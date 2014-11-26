a3e32831aeaf4edb06daf5b60f2a684647128f65
karel smejkal <smejkka3@fit.cvut.cz>
2014-11-26 20:56:19 -0200

    okno s napovedou... verze 1.0, mrkni an to pls adame a napis poznamky

--- src/cz/cvut/fit/project/noted/menus/actions/OpenHelpAction.java
+++ src/cz/cvut/fit/project/noted/menus/actions/OpenHelpAction.java
@@ -3,12 +3,16 @@ package cz.cvut.fit.project.noted.menus.actions;
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.GridLayout;
+import java.awt.Insets;
 import java.awt.Point;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JFrame;
@@ -16,6 +20,7 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
+import javax.swing.border.Border;
 public class OpenHelpAction extends JDialog implements ActionListener {
     
    private JFrame mainFrame;
@@ -27,19 +32,29 @@ public class OpenHelpAction extends JDialog implements ActionListener {
   }
   @Override
   public void actionPerformed(ActionEvent e) {
-    prepareGUI();
+    createGUI();
   }
   
-  private void prepareGUI(){
+  private void createGUI(){
       mainFrame = new JFrame("Help"); 
       mainFrame.setLayout(new GridLayout(3, 1));
+      mainFrame.setSize(350, 400);
        
       statusLabel = new JLabel("Help for NotEd", JLabel.CENTER);    
       
-      final JTextArea commentTextArea = 
-         new JTextArea("This help for NotEd.",5,20);
+      final JTextArea textArea = 
+         new JTextArea("Editace se provádí v dolní části aplikace v toolbaru s notami\n" +
+                        "V prvním toolbaru vyberete délku noty\n" +
+                        "V druhém poté k případnému výběru posuvky\n" +
+                         "\n" +
+                   " K editaci také můžete použít zkratky (enter na přidání noty\n"
+                 + " mezerník na přidání pomlky, šipkama pohybujete kurzorem,\n"
+                 + " backspace maže symbol před kurzorem, delete maže symbol\n"
+                 + " za kurzorem,\n"
+                 + " tabulátor vloží oddělovač taktu)",5,20);
+      textArea.setEditable(false);
 
-      JScrollPane scrollPane = new JScrollPane(commentTextArea); 
+      JScrollPane scrollPane = new JScrollPane(textArea);            
       
       closePanel = new JPanel();
       
@@ -50,13 +65,12 @@ public class OpenHelpAction extends JDialog implements ActionListener {
             mainFrame.dispose();        
          }
       }); 
-       
+      
       closePanel.add(button);
        
       mainFrame.add(statusLabel);
       mainFrame.add(scrollPane);
       mainFrame.add(closePanel);
-      mainFrame.pack();
       mainFrame.setLocationRelativeTo(null);
       mainFrame.setVisible(true);  
    }
