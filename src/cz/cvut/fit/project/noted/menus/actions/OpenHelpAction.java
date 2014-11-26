72037a4e60c3be7fe3300843e4f7ce82f8abb429
karel smejkal <smejkka3@fit.cvut.cz>
2014-11-26 14:21:26 -0200

    okno s napovedou, text, popr. Jtextfield jeste bude doplneno... still under construction

--- src/cz/cvut/fit/project/noted/menus/actions/OpenHelpAction.java
+++ src/cz/cvut/fit/project/noted/menus/actions/OpenHelpAction.java
@@ -1,23 +1,38 @@
 package cz.cvut.fit.project.noted.menus.actions;
 
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Point;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import javax.swing.JOptionPane;
-import java.awt.*; 
-import javax.swing.*; 
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+public class OpenHelpAction extends JDialog implements ActionListener {
+  
+  public OpenHelpAction(JFrame parent, String title) {
+    super(parent, title, true);
+    String message = "this is help";
 
-public class OpenHelpAction extends JPanel
-                      implements ActionListener 
-{
+    JPanel messagePane = new JPanel();
+    messagePane.add(new JLabel(message));
+    getContentPane().add(messagePane);
+    JPanel buttonPane = new JPanel();
+    JButton button = new JButton("Close"); 
+    buttonPane.add(button); 
+    button.addActionListener(this);
+    getContentPane().add(buttonPane, BorderLayout.SOUTH);
+    setDefaultCloseOperation(DISPOSE_ON_CLOSE);
 
-    public OpenHelpAction()
-    {
-    }
-
-    @Override
-    public void actionPerformed(ActionEvent e)
-    {
-            Help.createAndShowGUI();
-
-    }
+    pack(); 
+    setLocationRelativeTo(null);
+  }
+  @Override
+  public void actionPerformed(ActionEvent e) {
+    setVisible(true); 
+    dispose(); 
+  }
+  
 }
